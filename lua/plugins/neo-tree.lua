db8eff9919fea2a62a9cd32b7ac8c76cf7849281
yaroslav <yaroslav.d.chernik@gmail.com>
2024-10-21 21:39:20 +0300

    add tailwind-tools and update neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,13 +1,26 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
     require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          always_show_by_pattern = {
+            ".env*",
+          },
+          hide_by_name = {
+            "node_modules",
+            ".git",
+          },
+        },
+      },
       event_handlers = {
         {
           event = "file_open_requested",
@@ -17,7 +30,7 @@ return {
         },
       }
     })
-		vim.keymap.set({'n', 'v'}, "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-		vim.keymap.set({'n', 'v'}, "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-	end,
+    vim.keymap.set({ 'n', 'v' }, "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+    vim.keymap.set({ 'n', 'v' }, "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+  end,
 }
