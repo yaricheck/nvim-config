e7e8fdec5aceaffba1324cd84b0a6e72e05d803c
yaricheck <yaroslav.d.chernik@gmail.com>
2024-11-11 12:42:40 +0300

    add harpoon

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,20 @@
+return {
+	"theprimeagen/harpoon",
+	config = function()
+		local mark = require("harpoon.mark")
+		local ui = require("harpoon.ui")
+
+		vim.keymap.set("n", "<leader>a", mark.add_file)
+		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+		vim.keymap.set("n", "<C-a>", function()
+			ui.nav_prev()
+		end)
+		vim.keymap.set("n", "<C-V>", function()
+			ui.nav_next()
+		end)
+		vim.keymap.set("n", "<leader>p", ":Telescope harpoon marks<CR>")
+    require("telescope").load_extension('harpoon')
+    
+	end,
+}
