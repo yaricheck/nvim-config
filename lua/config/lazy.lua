7b9a9c367e9c4bb1c48c96f46daecd4829bbc65d
yaricheck <yaroslav.d.chernik@gmail.com>
2025-05-04 21:38:24 +0300

    add langmapper

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,5 +22,5 @@ require("lazy").setup({
     { import = "plugins" },
   },
   install = { colorscheme = { "habamax" } },
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
 })
