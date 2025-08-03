83a873ff424bd6c205e842d67c76c6ac2bf24a84
yaricheck <yaroslav.d.chernik@gmail.com>
2025-08-03 22:57:10 +0300

    remove linewrap
    i hate it

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,14 +1,13 @@
 vim.g.mapleader = " "
 vim.g.background = "light"
 
-local opt = vim.opt
-
-opt.number = true
-opt.relativenumber = true
-opt.expandtab = true
-opt.tabstop = 2
-opt.softtabstop = 2
-opt.shiftwidth = 2
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.wrap = false
 
 vim.opt.swapfile = false
 
@@ -21,4 +20,3 @@ vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.cmd("highlight LineNr guibg=none guifg=#e5e5e5")
 vim.wo.number = true
-
