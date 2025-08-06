cee7541eb1cd43771ec564702064cf203f3c2df2
yaroslav <yaroslav.d.chernik@gmail.com>
2025-08-06 11:16:28 +0300

    add macro to create sql insert statements

--- lua/macros.lua
+++ lua/macros.lua
@@ -1,3 +1,5 @@
 local map = vim.keymap.set
 
+vim.fn.setreg("l", "vF$€ý5yf €ý5lviwp€üA")
 map("n", "<leader>qc", ":g//normal dgn*PvB#<CR>")
+map("n", "<leader>csq", "^y$oINSERT INTO entities (pa)oVALUES (pa)%lciw$11000@l")
